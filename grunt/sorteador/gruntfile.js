db296da2304ed07631a28e304d7199d68f69ca17
Lorran Kaique <lksf214@gmail.com>
2025-02-12 22:01:56 -0400

    add pasta sorteador

---
+++ grunt/sorteador/gruntfile.js
@@ -0,0 +1,123 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                    //para onde vai : de onde veio
+                }
+            },
+            production: {
+                option: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
+            }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist:{
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.html' : 'src/index.html'
+                }
+            }
+        },
+        clean: ['prebuild']
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production','htmlmin:dist', 'replace:dist','clean']);
+
+    //começando a instalar o grunt:
+    /* 
+    no terminal central:
+    npm i -g grunt-cli
+    npm init
+    npm i --save-dev grunt
+
+    no terminal local:
+    npm run grunt
+
+        para instalar o less:
+        npm install --save-dev grunt-contrib-less
+
+        para instalar o sass:
+        npm install --save-dev grunt-contrib-sass
+
+        para executar o grunt de forma paralela:
+        npm install --save-dev grunt-concurrent
+
+        para instalar o watch:
+        npm install --save-dev grunt-contrib-watch
+
+        direcionar a referecia css tanto para deve quanto p dist:
+        npm install --save-dev grunt-replace
+        rodar: npm run grunt replace:dev
+
+        minificação do html:
+        npm install --save-dev grunt-contrib-htmlmin
+
+        apagar pasta temporária:
+        npm install --save-dev grunt-contrib-clean
+    */
+}
