319b30d7b05dab6b16f8b3a82069af1863a7d08a
Lorran Kaique <lksf214@gmail.com>
2025-02-13 17:42:34 -0400

    add pasta do exerc de pag de sortear numero

--- grunt/sorteador/gruntfile.js
+++ grunt/sorteador/gruntfile.js
@@ -5,11 +5,10 @@ module.exports = function(grunt) {
             development: {
                 files: {
                     'dev/styles/main.css': 'src/styles/main.less'
-                    //para onde vai : de onde veio
                 }
             },
             production: {
-                option: {
+                options: {
                     compress: true,
                 },
                 files: {
@@ -34,6 +33,10 @@ module.exports = function(grunt) {
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
                         }
                     ]
                 },
@@ -52,6 +55,10 @@ module.exports = function(grunt) {
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
                         }
                     ]
                 },
@@ -66,17 +73,24 @@ module.exports = function(grunt) {
             }
         },
         htmlmin: {
-            dist:{
+            dist: {
                 options: {
                     removeComments: true,
                     collapseWhitespace: true
                 },
                 files: {
-                    'prebuild/index.html' : 'src/index.html'
+                    'prebuild/index.html': 'src/index.html'
                 }
             }
         },
-        clean: ['prebuild']
+        clean: ['prebuild'],
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
@@ -84,40 +98,43 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production','htmlmin:dist', 'replace:dist','clean']);
-
-    //começando a instalar o grunt:
-    /* 
-    no terminal central:
-    npm i -g grunt-cli
-    npm init
-    npm i --save-dev grunt
-
-    no terminal local:
-    npm run grunt
-
-        para instalar o less:
-        npm install --save-dev grunt-contrib-less
-
-        para instalar o sass:
-        npm install --save-dev grunt-contrib-sass
-
-        para executar o grunt de forma paralela:
-        npm install --save-dev grunt-concurrent
-
-        para instalar o watch:
-        npm install --save-dev grunt-contrib-watch
-
-        direcionar a referecia css tanto para deve quanto p dist:
-        npm install --save-dev grunt-replace
-        rodar: npm run grunt replace:dev
-
-        minificação do html:
-        npm install --save-dev grunt-contrib-htmlmin
-
-        apagar pasta temporária:
-        npm install --save-dev grunt-contrib-clean
-    */
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
+//começando a instalar o grunt:
+/* 
+no terminal central:
+npm i -g grunt-cli
+npm init
+npm i --save-dev grunt
+
+no terminal local:
+npm run grunt
+
+    para instalar o less:
+    npm install --save-dev grunt-contrib-less
+
+    para instalar o sass:
+    npm install --save-dev grunt-contrib-sass
+
+    para executar o grunt de forma paralela:
+    npm install --save-dev grunt-concurrent
+
+    para instalar o watch:
+    npm install --save-dev grunt-contrib-watch
+
+    direcionar a referecia css tanto para deve quanto p dist:
+    npm install --save-dev grunt-replace
+    rodar: npm run grunt replace:dev
+
+    minificação do html:
+    npm install --save-dev grunt-contrib-htmlmin
+
+    apagar pasta temporária:
+    npm install --save-dev grunt-contrib-clean
+
+    baixar biblio Math:
+
+*/
